aa67729320d64d1d39f8a9d7a98b69220604925f
kathlen Salomon <kaatymomon@gmail.com>
2024-12-14 01:58:22 -0300

    Meu projeto conta banco

---
+++ javadoc/src/Calculadora.java
@@ -0,0 +1,22 @@
+/** 
+* <h1>Calculadora <p>
+* A Calculadora realiza operações matemáticas entre números
+* <p>
+* <b> Note: Leia atentamente a documentação desta classes
+* para desfrutar dos recursos oferecidos pelo autor
+*
+*@author Kathlen Salomon
+*@version 1.0
+*@since 01/01/2022
+*/
+public class Calculadora {
+   /** 
+    * Este método é utilizado para somar dois números inteiros
+    * @param numeroUm este é o primeiro parâmetro do método
+    * @param numeroDois este é o segundo parâmetro do método
+    * @return int o resultado deste método é a soma dos dois
+    */
+    public int somar (int numeroUm, int numeroDois){
+        return  numeroUm + numeroDois;
+    }
+}
---
+++ javadoc/src/Comentarios.java
@@ -0,0 +1,22 @@
+public class Comentarios{
+    public static void main(String[] args) {
+        //olá eu sou um comentário em uma unica linha;
+
+        /*Olá
+        * eu sou um comentário
+        *mais detalhado 
+         */ 
+
+         /**  Estas duas estrelinhas acima
+          * são para identificar que você
+          * pretende elaborar um comentário
+          * a nivel de coumentação.
+          * Que incrivel!!!
+          */
+
+          public void metodo(){
+
+          }
+    }
+}
+
